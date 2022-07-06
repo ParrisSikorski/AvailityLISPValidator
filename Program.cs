e88bced5c27cc5d390fc1382f38f3ff7703a7e87
Parris Sikorski <pm_sikorski@yahoo.com>
2022-07-06 09:16:41 -0700

    checking in final commit for the lisp validator

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AvailityLISPValidator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                var validator = new Validator();
+                var isValid = validator.IsValid(args[0]);
+            }
+        }
+    }    
+}
---
+++ Validator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AvailityLISPValidator
+{
+    public class Validator
+    {
+
+
+        public bool IsValid(string input)
+        {
+            Stack<char> s = new Stack<char>();
+            char[] arr = input.ToCharArray();
+            int index = 0;
+
+            while (index < input.Length)
+            {
+                if (arr[index] == '{' || arr[index] == '[' || arr[index] == '{')
+                {
+                    s.Push(arr[index]);
+                }
+
+                if (arr[index] == '}' || arr[index] == ']' || arr[index] == ')')
+                {
+                    if (s.Count == 0)
+                    {
+                        return false;
+                    }
+
+                    else if (IsMatch(s.Pop(), arr[index]))
+                    {
+                        return true;
+                    }
+
+                    return false;
+                }
+
+                index++;
+            }
+
+            if (s.Count == 0)
+            {
+                return false;
+            }
+
+            return false;
+        }
+
+        private bool IsMatch(char firstChar, char secondChar)
+        {
+            if (firstChar == '(' && secondChar == ')')
+            {
+                return true;
+            }
+            else if (firstChar == '[' && secondChar == ']')
+            {
+                return true;
+            }
+            else if (firstChar == '{' && secondChar == '}')
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
